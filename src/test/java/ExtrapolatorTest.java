1aa9d3127f1b7b47811ada1bae5a83f34e383426
Vladislav Makarov <v-makarov@yandex.ru>
2021-02-08 20:57:51 +0300

    Поправил тест ExtrapolatorTest

--- src/test/java/ExtrapolatorTest.java
+++ src/test/java/ExtrapolatorTest.java
@@ -5,10 +5,6 @@ class ExtrapolatorTest {
 
     @org.junit.jupiter.api.Test
     void extrapolate() {
-    }
-
-    @Test
-    public void calcExtrapolated() {
         // Образец набора данных для экстраполяции
         double[][] d = {{1.2, 2.7}, {1.4, 3.1}};
         // Пример значения x
@@ -16,4 +12,5 @@ class ExtrapolatorTest {
         System.out.println("Value of y at x = 2.1 : " + Extrapolator.extrapolate(d, x));
         Assertions.assertNotNull(d);
     }
+
 }
