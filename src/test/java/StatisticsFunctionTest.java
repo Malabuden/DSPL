1cc6a4608eb6c60ba41d5856515f99253565209c
Vladislav Makarov <v-makarov@yandex.ru>
2021-02-08 10:24:45 +0300

    Добавил заготовку теста

---
+++ src/test/java/StatisticsFunctionTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsFunctionTest {
+
+    @Test
+    void calcSignalMean() {
+    }
+
+    @Test
+    void calcSignalVariance() {
+    }
+
+    @Test
+    void calcSignalStd() {
+    }
+
+    @Test
+    void calcSignalMeanLoop() {
+    }
+
+    @Test
+    void calcSignalVarianceLoop() {
+    }
+
+    @Test
+    void calcSignalStdLoop() {
+    }
+
+    @Test
+    void calcSignalRms() {
+    }
+
+    @Test
+    void calcSigMin() {
+    }
+}
