0e6676051005d942019daab8ff46cab51d2ce8f6
Vladislav Makarov <v-makarov@yandex.ru>
2021-02-08 23:04:08 +0300

    Перенес все проверки в тесты

--- src/test/java/StatisticsFunctionTest.java
+++ src/test/java/StatisticsFunctionTest.java
@@ -1,38 +1,85 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class StatisticsFunctionTest {
 
     @Test
     void calcSignalMean() {
+        Signal inputSignal = new Signal();
+        double mean;
+        mean = StatisticsFunction.calcSignalMean(inputSignal.InputSignal_f32_1kHz_15kHz, inputSignal.InputSignal_f32_1kHz_15kHz.length);
+        System.out.println("Mean : ");
+        System.out.println(mean);
     }
 
     @Test
     void calcSignalVariance() {
+        Signal inputSignal = new Signal();
+        double variance;
+        double mean;
+        mean = StatisticsFunction.calcSignalMean(inputSignal.InputSignal_f32_1kHz_15kHz, inputSignal.InputSignal_f32_1kHz_15kHz.length);
+        variance = StatisticsFunction.calcSignalVariance(inputSignal.InputSignal_f32_1kHz_15kHz, mean, inputSignal.InputSignal_f32_1kHz_15kHz.length);
+        System.out.println("Variance : ");
+        System.out.println(variance);
     }
 
     @Test
     void calcSignalStd() {
+        Signal inputSignal = new Signal();
+        double std;
+        double variance;
+        double mean;
+        mean = StatisticsFunction.calcSignalMean(inputSignal.InputSignal_f32_1kHz_15kHz, inputSignal.InputSignal_f32_1kHz_15kHz.length);
+        variance = StatisticsFunction.calcSignalVariance(inputSignal.InputSignal_f32_1kHz_15kHz, mean, inputSignal.InputSignal_f32_1kHz_15kHz.length);
+        std = StatisticsFunction.calcSignalStd(variance);
+        System.out.println("Standard Deviation : ");
+        System.out.println(std);
     }
 
     @Test
     void calcSignalMeanLoop() {
+        Signal inputSignal = new Signal();
+        double meanLoop;
+        meanLoop = StatisticsFunction.calcSignalMeanLoop(inputSignal.InputSignal_f32_1kHz_15kHz, inputSignal.InputSignal_f32_1kHz_15kHz.length);
+        System.out.println("Mean Loop : ");
+        System.out.println(meanLoop);
+
     }
 
     @Test
     void calcSignalVarianceLoop() {
+        Signal inputSignal = new Signal();
+        double varianceLoop;
+        varianceLoop = StatisticsFunction.calcSignalVarianceLoop(inputSignal.InputSignal_f32_1kHz_15kHz, inputSignal.InputSignal_f32_1kHz_15kHz.length);
+        System.out.println("Variance Loop : ");
+        System.out.println(varianceLoop);
     }
 
     @Test
     void calcSignalStdLoop() {
+        Signal inputSignal = new Signal();
+        double stdLoop;
+        stdLoop =  StatisticsFunction.calcSignalStdLoop(inputSignal.InputSignal_f32_1kHz_15kHz,inputSignal.InputSignal_f32_1kHz_15kHz.length);
+        System.out.println("Standard Deviation Loop : ");
+        System.out.println(stdLoop);
     }
 
     @Test
     void calcSignalRms() {
+        Signal inputSignal = new Signal();
+        double rmsLoop;
+        rmsLoop = StatisticsFunction.calcSignalRms(inputSignal.InputSignal_f32_1kHz_15kHz, inputSignal.InputSignal_f32_1kHz_15kHz.length);
+        System.out.println("RMS : ");
+        System.out.println(rmsLoop);
     }
 
     @Test
     void calcSigMin() {
+        Signal inputSignal = new Signal();
+        double[] sigMinimum = new double[2];
+        sigMinimum = StatisticsFunction.calcSigMin(inputSignal.InputSignal_f32_1kHz_15kHz, inputSignal.InputSignal_f32_1kHz_15kHz.length);
+        System.out.print("Index of min value : ");
+        System.out.print(sigMinimum[1]);
+        System.out.print(" Min value : ");
+        System.out.println(sigMinimum[0]);
     }
 }
