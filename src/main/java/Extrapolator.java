89f8f50298baa2fe8a995465a2065421b6634568
Vladislav Makarov <v-makarov@yandex.ru>
2021-02-12 21:23:21 +0300

    Первая модель экстраполятора

--- src/main/java/Extrapolator.java
+++ src/main/java/Extrapolator.java
@@ -3,14 +3,74 @@
 
 public class Extrapolator {
 
-// Функция для вычисления экстраполяции сигнала
+// Функция для вычисления линейной экстраполяции сигнала
 
-   public static double extrapolate(double[][] d, double x) {
+    public static double LinExtrapolate(double[][] d, double x) {
 
         double y = d[0][1] + (x - d[0][0]) /
                 (d[1][0] - d[0][0]) *
                 (d[1][1] - d[0][1]);
         return y;
+    }
+
+    /**
+     * Вычисляет два коээфициента модели нелинейной регресии
+     *
+     * @param x массив входных значений по оси Х
+     * @param y массив входных значений по оси У
+     * @return возвращает два коээфициента функции нелинейной регрессии
+     */
+    public static double[] regressionsExtrapolator(double[] x, double[] y) {
+        double[] regressionCoefficients = new double[2];
+        double A;
+        double a;
+        double b;
+        double[] Z = new double[x.length];
+        double[] U = new double[x.length];
+        double[] Z2 = new double[x.length];
+        double[] ZU = new double[x.length];
+        double Zs = 0;
+        double Us = 0;
+        double Z2s = 0;
+        double ZUs = 0;
+
+        for (int i = 0; i < x.length; i++) {
+            Z[i] = Math.log10(x[i]);
+            U[i] = Math.log10(y[i]);
+            Z2[i] = Z[i] * Z[i];
+            ZU[i] = Z[i] * U[i];
+        }
+
+        for (int i = 0; i < x.length; i++) {
+            Zs += Z[i];
+            Us += U[i];
+            Z2s += Z2[i];
+            ZUs += ZU[i];
+        }
+
+        A = (Us * Z2s - ZUs * Zs) / (x.length * Z2s - Math.pow(Zs, 2));
+        a = Math.pow(10, A);
+        b = (x.length * ZUs - Zs * Us) / (x.length * Z2s - Math.pow(Zs, 2));
+
+        regressionCoefficients[0] = a;
+        regressionCoefficients[1] = b;
+
+        return regressionCoefficients;
+    }
+
+    /**
+     * @param x
+     * @param y
+     * @param xExtrapolation
+     * @return
+     */
+    public static double[] regressionFunction(double[] x, double[] y, double[] xExtrapolation) {
+        double[] yExtrapolation = new double[xExtrapolation.length];
+        double[] coefficients = Extrapolator.regressionsExtrapolator(x, y);
+        for (int i = 0; i < xExtrapolation.length; i++) {
+            yExtrapolation[i] = coefficients[0] * Math.pow(xExtrapolation[i], coefficients[1]);
+        }
+        return yExtrapolation;
     }
 
 }
--- src/test/java/ExtrapolatorTest.java
+++ src/test/java/ExtrapolatorTest.java
@@ -1,4 +1,3 @@
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 class ExtrapolatorTest {
@@ -9,8 +8,39 @@ class ExtrapolatorTest {
         double[][] d = {{1.2, 2.7}, {1.4, 3.1}};
         // Пример значения x
         double x = 3.1;
-        System.out.println("Value of y at x = 2.1 : " + Extrapolator.extrapolate(d, x));
+        System.out.println("Value of y at x = 2.1 : " + Extrapolator.LinExtrapolate(d, x));
         Assertions.assertNotNull(d);
+    }
+
+    @org.junit.jupiter.api.Test
+    void regressionCoefficientsTest() {
+        double[] x = {1, 2, 3, 4, 5, 6};
+        double[] y = {4, 6, 8, 9, 10, 11};
+
+        double[] yExtrapolation = new double[x.length];
+        double[] coefficients = Extrapolator.regressionsExtrapolator(x, y);
+        for (double coefficient : coefficients) {
+            System.out.println(coefficient);
+        }
+
+        System.out.println("Восстановленная кривая");
+        for (int i = 0; i < x.length; i++) {
+            yExtrapolation[i] = coefficients[0] * Math.pow(x[i], coefficients[1]);
+            System.out.println(yExtrapolation[i]);
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void regressionFunctionTest() {
+        double[] x = {1, 2, 3, 4, 5, 6, 8, 11};
+        double[] y = {4, 6, 8, 9, 10, 11, 10, 12};
+        double[] xExtrap = {12, 13, 14, 15, 16, 17};
+
+        double[] regressionValues = new double[xExtrap.length];
+        regressionValues = Extrapolator.regressionFunction(x, y, xExtrap);
+        for (int i = 0; i < xExtrap.length; i++) {
+            System.out.println(regressionValues[i]);
+        }
     }
 
 }
